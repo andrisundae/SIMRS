8e0e899c5be65f209371e284d15f8379419396a9
Mohamad Saiful <fren_yaif@yahoo.com>
2021-01-26 15:02:26 +0700

    app/billing - hapus route diagnosis-ix & diagnosis-x
    app/rekam-medis
    - tambah route referensi-umum
    - optimasi style saat menggunakan .inline.fields dari semantic-ui
    
    #27

--- app/rekam-medis/src/routers.desktop.js
+++ app/rekam-medis/src/routers.desktop.js
@@ -5,6 +5,11 @@ import mobileRouters from './routers.mobile';
 export default [
   ...mobileRouters,
   {
+    key: '_billing_master_referensi_umum',
+    path: '/billing/master/referensi-umum',
+    component: lazy(() => import('@module/referensi-umum')),
+  },
+  {
     key: '_rekam_medis_icd10',
     path: '/master/icd10',
     component: lazy(() => import('@module/icd10')),
